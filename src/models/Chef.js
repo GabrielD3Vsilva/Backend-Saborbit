ff322bfc009ec1f510d1ed77831564d967ecd236
Gabriel-developments <123119325+GabrielD3Vsilva@users.noreply.github.com>
2025-06-30 22:36:14 -0300

    Update Chef.js

--- src/models/Chef.js
+++ src/models/Chef.js
@@ -37,7 +37,7 @@ const chefSchema = new mongoose.Schema({
     },
     planoAtivo: { 
       type: Boolean,
-      default: true,
+      default: false,
     },
     dataExpiracaoPlano: { 
       type: Date,
